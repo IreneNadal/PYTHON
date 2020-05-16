8662fe48a213569d18dfdc0ecac2da619146b112
Irene Nadal <irene.nadalarizo@gmail.com>
2020-05-16 12:21:03 +0200

    He hecho el kata 1

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,10 @@
+# COMMENT = CTRL + K + C
+# UNCOMMENT = CTRL + K + U
 import os 
 os.system("cls") # Clear screen in WINDOWS
-
+import statistics 
+import random
+import array as arr 
 import math 
 print('hello, world')
 if (13!=13):
@@ -11,9 +15,6 @@ else:
 cociente = 7/3
 resto = 7%3
 mult = 7*3
-print(math.sqrt(8))
-print(math.isqrt(8))
-print(cociente)
 
 # Funcion numeros pares
 def es_par(x):
@@ -21,7 +22,12 @@ def es_par(x):
         print('es par')
     else:
         print('es impar')
-es_par(2)
+#es_par(2)
+
+# Funcion haz el cuadrado de x
+def cuadrado(x):
+    return x*x
+print(cuadrado(2))
 
 # Funcion números primos 
 def es_primo(x):
@@ -35,7 +41,7 @@ def es_primo(x):
         print('El numero no es primo')
     else:
         print('El numero es primo')
-es_primo(8)
+#es_primo(8)
 
 
 # Función para saber si el color es rgb (red - green - blue)
@@ -49,6 +55,50 @@ def is_rgb(color):
         print('Color es RGB')
     else:
         print('El color no es RGB')
-is_rgb("rojo")
+#is_rgb("rojo")
 
-print('Estoy añadiendo algo nuevo al commit')
+
+# Función para saber que nota has sacado
+def is_pass():
+    notas = [random.random()*10 for x in range(20)]
+    for x in notas:
+        if(x<4.5):
+            print(round(x,2),'Has supendido, compañero')
+        elif(x<5):
+            print(round(x,2),'Te hago media')
+        elif(x<6):
+            print(round(x,2),'Suficiente')
+        elif(x<9.5):
+            print(round(x,2),'Tienes un notable')
+        elif(x<10):
+            print(round(x,2),'Matrícula de honor')
+    m = statistics.mean(notas)
+    print("La media de la clase es",round(m,2))
+is_pass()
+
+def is_watermass(temp,salt):
+    if temp >= 17.3 and temp <= 17.8 and salt >= 38.3 and salt <= 38.8:
+        print('Water mass is WIW') 
+    elif temp >= 16.9 and temp <= 17.4 and salt >= 36.5 and salt <= 37.1:
+        print('Water mass is SAW')
+    else:
+        print('I have not found any water mass')
+
+is_watermass(12.5,38.5)
+
+# # price of PAN
+# precio = 3.49
+# descuento = 0.60
+# precio_con_descuento = precio*(1-descuento)
+# barras_vendidas = input('Cuantas barras de HOY has vendido:')
+# barras_ayer = input('Cuantas barras de AYER has vendido:')
+# print((int(barras_vendidas)-int(barras_ayer))*3.49 + int(barras_ayer)*precio_con_descuento)
+
+
+# CONTRASEÑA
+password = 'mango'
+password_usuario = input('Introduzca su contraseña: ')
+if password_usuario == password:
+    print('Contraseña correcta')
+else:
+    print('Contraseña incorrecta')
